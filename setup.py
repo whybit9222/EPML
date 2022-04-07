93e45ff9130464607313840e4ce27dbb2799f360
whybit9222 <59044352+whybit9222@users.noreply.github.com>
2022-04-08 06:35:17 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(  
+    name='EPML', 
+    version='0.9.0',
+    description='An open-source python package predicting the explosion limit of Hydrogen-Oxygen-Nitrogen mixtures.',
+    author='w.h.y.',
+    author_email='why_bit@bit.edu.cn',
+    url='https://github.com/whybit9222/EPML',
+    packages=['EPML'],
+)
